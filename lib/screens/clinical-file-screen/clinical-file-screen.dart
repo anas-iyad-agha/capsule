16d8e2eeaa4f6bad42b2863c122c3e9ee0729e28
anas-iyad-agha <anasagha2001@gmail.com>
2025-12-06 22:03:06 +0300

    redesigned the clinical file page

--- lib/screens/clinical-file-screen/clinical-file-screen.dart
+++ lib/screens/clinical-file-screen/clinical-file-screen.dart
@@ -1,5 +1,6 @@
 import 'package:Capsule/screens/clinical-file-screen/patient-info-screen/patient-info-screen.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class ClinicalFileScreen extends StatefulWidget {
   static const route = '/clinical-file';
@@ -14,19 +15,65 @@ class _ClinicalFileScreenState extends State<ClinicalFileScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('الملف السريري')),
-      body: Column(
-        children: [
-          GestureDetector(
-            onTap: () {
-              Navigator.pushNamed(context, PatientInfoScreen.route);
-            },
-            child: Text('معلومات المريض'),
-          ),
-          GestureDetector(child: Text('الأدوية')),
-          GestureDetector(child: Text('الأمراض')),
-          GestureDetector(child: Text('العمليات')),
-          GestureDetector(child: Text('التحاليل')),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Card(
+              child: ListTile(
+                minVerticalPadding: 32,
+                onTap: () =>
+                    Navigator.pushNamed(context, PatientInfoScreen.route),
+                trailing: Icon(Icons.arrow_forward_ios),
+                leading: Icon(Icons.person, color: Colors.cyan),
+                title: Text('معلومات المريض'),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                minVerticalPadding: 32,
+                onTap: () =>
+                    Navigator.pushNamed(context, PatientInfoScreen.route),
+                trailing: Icon(Icons.arrow_forward_ios),
+                leading: FaIcon(FontAwesomeIcons.pills, color: Colors.cyan),
+                title: Text('الأدوية'),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                minVerticalPadding: 32,
+                onTap: () =>
+                    Navigator.pushNamed(context, PatientInfoScreen.route),
+                trailing: Icon(Icons.arrow_forward_ios),
+                leading: FaIcon(FontAwesomeIcons.virus, color: Colors.cyan),
+                title: Text('الامراض'),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                minVerticalPadding: 32,
+                onTap: () =>
+                    Navigator.pushNamed(context, PatientInfoScreen.route),
+                trailing: Icon(Icons.arrow_forward_ios),
+                leading: FaIcon(
+                  FontAwesomeIcons.stethoscope,
+                  color: Colors.cyan,
+                ),
+                title: Text('العمليات'),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                minVerticalPadding: 32,
+                onTap: () =>
+                    Navigator.pushNamed(context, PatientInfoScreen.route),
+                trailing: Icon(Icons.arrow_forward_ios),
+                leading: FaIcon(FontAwesomeIcons.flask, color: Colors.cyan),
+                title: Text('التحاليل'),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
